a9045477387b59e63d4c3c817de98c6c562773a4
badeleux <badeleux@gmail.com>
2015-05-08 09:18:24 +0200

    Initial Commit

---
+++ Typhoon-ScopesExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Typhoon-ScopesExample
+//
+//  Created by Kamil Badyla on 08/05/15.
+//  Copyright (c) 2015 Badeleux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Typhoon-ScopesExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Typhoon-ScopesExample
+//
+//  Created by Kamil Badyla on 08/05/15.
+//  Copyright (c) 2015 Badeleux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
